fdaf9b9d0c408ba5af5d61b3e7a2a32718b8938e
Gleb Ignatovich <g.ignatovich@a1qa.com>
2021-03-11 10:07:16 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -8,8 +8,8 @@ namespace Task3
     class Program
     {
         private const string extension = ".txt";
-        private const string path = @"D:\GlebIgnatovich\VisualStudio\Task3\Task3\Files\";
-        private const string name = "1";
+        private static string path;
+        private static string name;
         static void Main(string[] args)
         {
             string resultPath = SelectСases(path + name + extension, 10);
@@ -19,18 +19,19 @@ namespace Task3
         public static string SelectСases(string pathToFirstFile, int amountOfStrings)
         {
             string newPath = string.Format("{0}{1}_res{2}", path, name, extension);
-           /* Console.WriteLine(newPath);
-            using (FileStream fstream = File.OpenRead(pathToFirstFile))
+            using (StreamReader sr = new StreamReader(pathToFirstFile, System.Text.Encoding.Default))
             {
-                byte[] array = new byte[fstream.Length];
-                fstream.Read(array, 0, array.Length);
-                string textFromFile = System.Text.Encoding.Default.GetString(array);
-                
-            }*/
+                string line;
+                Console.WriteLine("Start file data:");
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
             var file = new List<string>(System.IO.File.ReadAllLines(pathToFirstFile));
-            var file2 = new List<string>();
-            Random random = new Random();
+            var file2 = new List<string>();           
             file2.Add(file[0]);
+            Random random = new Random();
             for (int i = 1; i < amountOfStrings; i++)
             {
                 int index = random.Next(1, file.Count);
@@ -39,7 +40,6 @@ namespace Task3
             }          
             File.WriteAllLines(newPath, file2.ToArray());
             File.WriteAllLines(pathToFirstFile, file.ToArray());
-
             Console.ReadLine();
             return newPath;
         }
